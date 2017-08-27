9ed271532e63596c1229e6119c128c7d89310a77
Ashley Stewart <ashley.j.stewart@live.com>
2017-08-27 23:18:20 +1200

    drag drop added

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 fs = require('fs'),
 http = require('http'),
 csv = require('csv')
-express = require('express')
+dragDrop = require('drag-drop')
 
-app = express();
+dragDrop('#dropTarget', function (files) {
+    console.log('Here are the dropped files', files)
+
+    files.forEach(function (file) {
+        console.log(file.name)
+        console.log(file.size)
+        console.log(file.type)
+        console.log(file.lastModifiedData)
+        console.log(file.fullPath)
+
+    })
+});
